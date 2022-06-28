d6f15c58d073cfc4f6eec0e8244cff58a5f8f1e8
Cientista-Avogadro <cientistaavogadro2019@gmail.com>
2022-06-28 06:39:25 +0100

    finished projecto

--- firebase.ts
+++ firebase.ts
@@ -44,22 +44,26 @@ const signInWithGoogle = async () => {
         email: user.email,
       });
     }
-  } catch (err) {
+  } catch (err: any) {
     console.error(err);
     alert(err.message);
   }
 };
 
-const logInWithEmailAndPassword = async (email, password) => {
+const logInWithEmailAndPassword = async (email: string, password: string) => {
   try {
     await signInWithEmailAndPassword(auth, email, password);
-  } catch (err) {
+  } catch (err: any) {
     console.error(err);
     alert(err.message);
   }
 };
 
-const registerWithEmailAndPassword = async (name, email, password) => {
+const registerWithEmailAndPassword = async (
+  name: string,
+  email: string,
+  password: string
+) => {
   try {
     const res = await createUserWithEmailAndPassword(auth, email, password);
     const user = res.user;
@@ -69,17 +73,17 @@ const registerWithEmailAndPassword = async (name, email, password) => {
       authProvider: 'local',
       email,
     });
-  } catch (err) {
+  } catch (err: any) {
     console.error(err);
     alert(err.message);
   }
 };
 
-const sendPasswordReset = async email => {
+const sendPasswordReset = async (email: string) => {
   try {
     await sendPasswordResetEmail(auth, email);
     alert('Password reset link sent!');
-  } catch (err) {
+  } catch (err: any) {
     console.error(err);
     alert(err.message);
   }
